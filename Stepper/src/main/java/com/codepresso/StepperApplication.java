8cff5992a80be7650ac27fb3b0685bfa3ce834e7
Hansol Tak <takyhsoul@gmail.com>
2020-03-08 04:24:17 +0900

    Create spring-boot project

---
+++ Stepper/src/main/java/com/codepresso/StepperApplication.java
@@ -0,0 +1,13 @@
+package com.codepresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepperApplication.class, args);
+	}
+
+}
---
+++ Stepper/src/test/java/com/codepresso/StepperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
